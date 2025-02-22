8c752593d6521d313dfec7810a27eda0bb857fdd
Nikhita Anusuri <nanusuri@charlotte.edu>
2025-02-22 09:34:32 -0500

    add exercise 5

---
+++ ex05-more-variables-and-printing/variables-and-printing.py
@@ -0,0 +1,18 @@
+name = 'Zed A. Shaw'
+age = 35 #not a lie
+height = 74 #inches
+height = height * 2.54
+weight = 180 #lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} cm tall.")
+print(f"He's {weight} pounds heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
+
+total = age + height +weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
