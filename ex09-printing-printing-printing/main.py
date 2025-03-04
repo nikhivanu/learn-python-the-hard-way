e6de4317a5a732d5f4fa7a8b227bbd4c1665e538
Nikhita <nikhi.v.anu@gmail.com>
2025-03-03 23:17:13 -0500

    add exercise 9

---
+++ ex09-printing-printing-printing/main.py
@@ -0,0 +1,15 @@
+#creates variables days and months
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJune\nJuly\nAug"
+
+#prints the variables
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+#using three double quotes allows for printing on 
+# multiple lines with one statement without \n
+
+print("""There's something going on here. 
+      With the three doublequotes.
+      We'll be able to type as much as we like.
+      Even 4 lines if we want, or 5, or 6.""")
