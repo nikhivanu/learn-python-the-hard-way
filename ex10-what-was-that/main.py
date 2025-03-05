e76212915316f76c46e8057d0a007af73ace5f3a
Nikhita <nikhi.v.anu@gmail.com>
2025-03-04 22:29:49 -0500

    add exercise 10

---
+++ ex10-what-was-that/main.py
@@ -0,0 +1,24 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+#prints multiple lines with one print statement
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+
+#seems like triple double quotes and triple single quotes
+#work the same way
+silly_cat = '''
+I'm testing out
+triple single quotes
+cats are so adorable
+'''
+print(silly_cat)
