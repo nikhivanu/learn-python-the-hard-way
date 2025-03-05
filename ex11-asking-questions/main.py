f4c7df56732c08ae7d491562ea8358e7b1a01306
Nikhita <nikhi.v.anu@gmail.com>
2025-03-05 18:05:27 -0500

    add exercise 11

---
+++ ex11-asking-questions/main.py
@@ -0,0 +1,14 @@
+print("How old are you?", end = ' ')
+age = input()
+print("How tall are you?", end = ' ')
+height = input()
+print("How much do you weigh?", end = ' ')
+weight = input()
+
+
+print("How are you feeling right now?")
+state = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy. You are feeling {state}")
+
+
