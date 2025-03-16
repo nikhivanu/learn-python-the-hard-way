9fa51735d8f1315e149152d7fbf32d4526e88d8c
Nikhita <nikhi.v.anu@gmail.com>
2025-03-16 09:53:48 -0400

    add exercise 34

---
+++ ex34-accessing-elements-of-list/main.py
@@ -0,0 +1,13 @@
+animals = ['bear', 'tiger', 'penguin', 'zebra']
+bear = animals[0]
+
+animals2 = ['bear', 'python3.13', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print(animals2[1])
+print(animals2[2])
+print(animals2[0])
+print(animals[3])
+print(animals2[4])
+print(animals2[2])
+print(animals2[5])
+print(animals2[4])
