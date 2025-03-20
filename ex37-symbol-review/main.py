03507f84d2f971abb7044b2705bf034123c4e381
Nikhita <nikhi.v.anu@gmail.com>
2025-03-20 07:26:26 -0400

    add exercise 37

---
+++ ex37-symbol-review/main.py
@@ -0,0 +1,6 @@
+print("We're going to test string escape sequences \\")
+
+print("\"")
+print("we can use \\ right before \" so the print statement still works")
+print("same works for \\ before \'")
+
